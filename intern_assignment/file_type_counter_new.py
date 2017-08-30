62fd32618338f87ba99fe5d4798fc19598f38e45
RaeesQ <raees.qaisir@workshare.com>
2017-08-30 17:44:58 +0100

    a change so that the result from the counter will make more sense when read

--- intern_assignment/file_type_counter_new.py
+++ intern_assignment/file_type_counter_new.py
@@ -6,4 +6,4 @@ counter = 0
 for name_of_file in dir_files:
     if name_of_file.startswith("WEB-") or name_of_file[0].isdigit():
         counter += 1
-print counter
+print counter, ":this is the amount of tests that are matching the accepted criteria"
