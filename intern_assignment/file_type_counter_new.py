0e165bcac22ed03ef7a37f940270996512d64648
RaeesQ <raees.qaisir@workshare.com>
2017-08-30 17:28:54 +0100

    This python file allows ou to find the files depedning on the prefix of the file name. it will fetch the files from the firectory specified in the terminal. the python file also needs to be in the same location for the files thatwill be searched through to be found

--- intern_assignment/file_type_counter_new.py
+++ intern_assignment/file_type_counter_new.py
@@ -1,27 +1,9 @@
-
-'''
-read files from directory
-loop over list of files
-if file name starts with a number or filename starts with "WEB-"
-   then increase counter
-else
-   do nothing
-
-questions:
-what type are the filenames?
-is there any specific method that does what is written in pseudo code?
-what does the "in" operator do in python?
-    
-'''
-
 import os
-path = "/home/raees/internship/intern_assignment/intern_assignment"
-
+my_directory=os.getcwd()
+path = my_directory
 dir_files = os.listdir(path)
-for file_count in dir_files:
-    print file_count
 counter = 0
-for file_count in dir_files:
-    if file_count.startswith("WEB-") or file_count[0].isdigit():
+for name_of_file in dir_files:
+    if name_of_file.startswith("WEB-") or name_of_file[0].isdigit():
         counter += 1
-        print counter
+print counter
