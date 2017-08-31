95d728e3e081a298a006be779a00e08b95c570a3
RaeesQ <raees.qaisir@workshare.com>
2017-08-31 10:05:09 +0100

    I have made changes to vairbale name path, this line had no affect of the code so it needed to be removed, code still works as it should

--- intern_assignment/file_type_counter_new.py
+++ intern_assignment/file_type_counter_new.py
@@ -1,9 +1,8 @@
 import os
 my_directory=os.getcwd()
-path = my_directory
-dir_files = os.listdir(path)
+dir_files = os.listdir(my_directory)
 counter = 0
 for name_of_file in dir_files:
     if name_of_file.startswith("WEB-") or name_of_file[0].isdigit():
         counter += 1
-print counter, ":this is the amount of tests that are matching the accepted criteria"
+print counter, ":this is the amount of test files that match the accepted criteria"
